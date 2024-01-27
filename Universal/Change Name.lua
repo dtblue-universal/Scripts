ffc89591822ce256e92c595605f31c2d435b1c6e
$ dtblue <94876822+dtblue-universal@users.noreply.github.com>
2024-01-27 11:03:24 +0800

    Update Change Name.lua

--- Universal/Change Name.lua
+++ Universal/Change Name.lua
@@ -1,3 +1,27 @@
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+    Title = "-- $ --";
+    Text = "Universal | bannedlux";
+    Icon = "rbxthumb://type=Asset&id=16126567466&w=150&h=150"})
+Duration = 5;
+
+wait(5)
+
 local Name = _G.User
 local player = game.Players[Name]
 player.DisplayName = _G.Cn
+
+wait(3)
+
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+    Title = "-- $ --";
+    Text = "Custom Name | bannedlux";
+    Icon = "rbxthumb://type=Asset&id=16126567466&w=150&h=150"})
+Duration = 5;
+
+wait(5)
+
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+    Title = "-- $ --";
+    Text = "Loaded.";
+    Icon = "rbxthumb://type=Asset&id=16126567466&w=150&h=150"})
+Duration = 16;
