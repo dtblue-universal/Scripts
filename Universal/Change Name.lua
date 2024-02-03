9c154615a54b6319a777dc58b79c8a6cfd7b2423
$ dtblue <94876822+dtblue-universal@users.noreply.github.com>
2024-02-04 04:11:38 +0800

    Create Change Name.lua

---
+++ Universal/Change Name.lua
@@ -0,0 +1,16 @@
+local user = game.Players:FindFirstChild(_G.Username)
+
+if user then
+	local displayName = user.DisplayName
+	local customDisplayName = user.DisplayName .. " " .. _G.Cn
+else
+	game:GetService("StarterGui"):SetCore("SendNotification", {
+		Title = "-- $ --",
+		Text = "Player Not found",
+		Icon = "rbxthumb://type=Asset&id=16126567466&w=150&h=150",
+		Duration = 5,
+		Button1 = "OK",
+		Callback = function()
+		end
+	})
+end
