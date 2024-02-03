45a46a745d8015be80b9606506f51660540824e0
$ dtblue <94876822+dtblue-universal@users.noreply.github.com>
2024-02-04 04:08:56 +0800

    Update Change Name.lua

--- Universal/Change Name.lua
+++ Universal/Change Name.lua
@@ -1,27 +1,14 @@
-game:GetService("StarterGui"):SetCore("SendNotification", { 
-    Title = "-- $ --";
-    Text = "Universal | bannedlux";
-    Icon = "rbxthumb://type=Asset&id=16126567466&w=150&h=150"})
-Duration = 5;
-
-wait(5)
-
-local Name = _G.User
-local player = game.Players[Name]
-player.DisplayName = _G.Cn
-
-wait(3)
-
-game:GetService("StarterGui"):SetCore("SendNotification", { 
-    Title = "-- $ --";
-    Text = "Custom Name | bannedlux";
-    Icon = "rbxthumb://type=Asset&id=16126567466&w=150&h=150"})
-Duration = 5;
-
-wait(5)
-
-game:GetService("StarterGui"):SetCore("SendNotification", { 
-    Title = "-- $ --";
-    Text = "Loaded.";
-    Icon = "rbxthumb://type=Asset&id=16126567466&w=150&h=150"})
-Duration = 16;
+if user then
+	local displayName = user.DisplayName
+	local customDisplayName = user.DisplayName .. " " .. _G.Cn
+else
+	game:GetService("StarterGui"):SetCore("SendNotification", {
+		Title = "-- $ --",
+		Text = "Player Not found",
+		Icon = "rbxthumb://type=Asset&id=16126567466&w=150&h=150",
+		Duration = 5,
+		Button1 = "OK",
+		Callback = function()
+		end
+	})
+end
