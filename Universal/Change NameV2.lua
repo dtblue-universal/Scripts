9ddca4ca86f3088e5f79aedaf1ff60b5a0b1ca13
$ dtblue <94876822+dtblue-universal@users.noreply.github.com>
2024-02-04 16:36:16 +0800

    Update Change NameV2.lua

--- Universal/Change NameV2.lua
+++ Universal/Change NameV2.lua
@@ -1,1 +1,13 @@
-
+if Player.Character then
+	if Player.Character.Parent.Name == 'Players' then
+		if _G.Crown then
+			Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑]' .. Player.DisplayName)
+		elseif _G.Verified then
+			Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[☑]' .. Player.DisplayName)
+		elseif _G.Star then
+			Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐]' .. Player.DisplayName)
+		elseif _G.StarLegend then
+			Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[🌟]' .. Player.DisplayName)
+		end
+	end
+end
