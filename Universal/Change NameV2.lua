a5cf5478bdcb525ee7527ce02042d6f3ad507930
$ dtblue <94876822+dtblue-universal@users.noreply.github.com>
2024-02-09 17:45:06 +0800

    Update Change NameV2.lua

--- Universal/Change NameV2.lua
+++ Universal/Change NameV2.lua
@@ -1,18 +1,18 @@
 if Player.Character then
-	if Player.Character.Parent.Name == 'Players' then
-		if _G.Crown then
-			Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑]' .. Player.DisplayName)
-		elseif _G.Verified then
-			Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[☑]' .. Player.DisplayName)
-		elseif _G.Star then
-			Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐]' .. Player.DisplayName)
-		elseif _G.StarLegend then
-			Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[🌟]' .. Player.DisplayName)
-		end
-	end
+    if Player.Character[_G.Username].Name == 'Players' then
+        if _G.Crown then
+            Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑]' .. Player.DisplayName)
+        elseif _G.Verified then
+            Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[☑]' .. Player.DisplayName)
+        elseif _G.Star then
+            Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐]' .. Player.DisplayName)
+        elseif _G.StarLegend then
+            Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[🌟]' .. Player.DisplayName)
+        end
+    end
 end
 
 game.StarterGui:SetCore("SendNotification", {
     Title = "-- $ --";
-    Text = "Lux Scripts.";
+    Text = "LuxScripts.";
 })
