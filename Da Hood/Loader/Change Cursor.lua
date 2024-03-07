0c475d25b3dcb1498ace39a692eceae9caec6b01
$ dtblue <94876822+dtblue-universal@users.noreply.github.com>
2024-03-08 00:18:45 +0800

    Update Change Cursor.lua

--- Da Hood/Loader/Change Cursor.lua
+++ Da Hood/Loader/Change Cursor.lua
@@ -1,4 +1,28 @@
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+    Title = "-- $ --";
+    Text = "Da Hood | bannedlux";
+    Icon = "rbxthumb://type=Asset&id=16126567466&w=150&h=150"})
+Duration = 5;
+
+wait(5)
+
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+    Title = "-- $ --";
+    Text = "Custom Cursor | bannedlux";
+    Icon = "rbxthumb://type=Asset&id=16126567466&w=150&h=150"})
+Duration = 5;
+
+wait(5)
+
 local player = game:GetService("Players").LocalPlayer
 local customCursor = player.Information.CustomCursor
 
 customCursor.Value = _G.CursorID
+
+wait(0.5)
+
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+    Title = "-- $ --";
+    Text = "Loaded.";
+    Icon = "rbxthumb://type=Asset&id=16126567466&w=150&h=150"})
+Duration = 16;
